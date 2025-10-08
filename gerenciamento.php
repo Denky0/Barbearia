132afec0157f3a62d49d6e6e0b1b87ee872270ef
Denky0 <allangui619@gmail.com>
2025-10-08 17:20:37 -0300

    refazendo o gerenciamento

--- gerenciamento.php
+++ gerenciamento.php
@@ -12,50 +12,36 @@ $pessoas = $conexao->query("SELECT * FROM pessoa");
 <h1>Gerenciamento</h1>
 <table class="table table-light table-striped">
     <thead>
-       <th scope="col">CLIENTE</th> 
+        <th scope="col">NOME</th>
+        <th scope="col">FUNÇÂO</th>
         <th scope="col">TELEFONE</th>
-        <th scope="col">SERVIÇO</th>
-        <th scope="col">DATA/HORA</th>
-        <th scope="col">STATUS</th>
         <th scope="col">AÇÔES</th>
     </thead>
-
     <tbody>
 
         <?php
-            if ($agendamentos->num_rows > 0){
-                while ($agendamento = $agendamentos->fetch_object()) {
-                    if ($pessoas->num_rows > 0){
-                        while($pessoa = $pessoas->fetch_object()) {
-                            echo "
-                                <tr>
-                                    <td>
-                                        $agendamento->cliente
-                                    </td>
-                                    <td>
-                                        $pessoa->telefone
-                                    </td>
-                                    <td>
-                                        $agendamento->servico
-                                    </td>
-                                    <td>    
-                                        $agendamento->dia |
-                                        $agendamento->hora
-                                    </td>
-                                    <td>  
-                                        $agendamento->status
-                                    </td>
-                                    <td>
-                                        <a href='editar.php?codigo=$agendamento->codigo' class='btn btn-success btn-sm'>Editar</a>
-                                        <a href='delete.php?codigo=$agendamento->codigo' class='btn btn-danger btn-sm'>Excluir</a>
-                                    </td>
-                                </tr>
-                            ";
-                        }
-                    }
-                    
+        if ($pessoas->num_rows > 0) {
+            while ($pessoa = $pessoas->fetch_object()) {
+                echo "
+                    <tr>
+                        <td>
+                            $pessoa->nome
+                        </td>
+                        <td>
+                            $pessoa->funcao
+                        </td>
+                        <td>
+                            $pessoa->telefone
+                        </td>
+                        <td>
+                            <a href='editarP.php?codigo=$pessoa->id' class='btn btn-success btn-sm'>Editar</a>
+                            <a href='deleteP.php?id=$pessoa->id' class='btn btn-danger btn-sm'>Excluir</a>
+                        </td>
+                    </tr>
+                ";
             }
-                }
+        }
+
 
         ?>
 
--- listarAgendamento.php
+++ listarAgendamento.php
@@ -10,8 +10,8 @@
         echo "<div class='card m-2' style='width: 18rem;'>
                 <img src='https://img.freepik.com/vetores-gratis/calendario-de-glifos-com-marca-de-verificacao_78370-7210.jpg?semt=ais_hybrid&w=740&q=80' class='card-img-top' alt='...'>
                 <div class='card-body'>
-                <p class='card-text'>Barbeiro: $agendamento->cliente</p>
-                <p class='card-text'>Cliente: $agendamento->barbeiro</p>
+                <p class='card-text'>Barbeiro: $agendamento->barbeiro</p>
+                <p class='card-text'>Cliente: $agendamento->cliente</p>
                 <p class='card-text'>Serviço: $agendamento->servico</p>
                 <p class='card-text'>Horário: $agendamento->hora</p>
                 <p class='card-text'>Dia: $agendamento->dia</p>
